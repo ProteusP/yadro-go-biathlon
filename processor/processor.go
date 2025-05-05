2588bf389e6a7f31f3cecd5455a70a170f000024
Mac <doremiheev@vk.com>
2025-05-05 17:11:41 +0300

    processing refactoring

--- processor/processor.go
+++ processor/processor.go
@@ -52,96 +52,37 @@ func (p *Processor) ProcessEvents() {
 
 		switch e.EventID {
 		case 1: // Registration
-			log := fmt.Sprintf("The competitor(%d) registered", e.CompetitorID)
-			p.AddLog(e.Time, log)
+			p.handleRegistration(e, comp)
 
 		case 2: // Start time
-			if len(e.ExtraParams) == 1 {
-				startTime := e.ExtraParams[0]
-				parsedStartTime, err := time.Parse("15:04:05.000", startTime)
-				comp.CurLapStart = parsedStartTime
-				if err == nil {
-					comp.PlannedStart = parsedStartTime
-					log := fmt.Sprintf("The start time of competitor(%d) was set by a draw to %s", e.CompetitorID, startTime)
-					p.AddLog(e.Time, log)
-				}
-			}
+			p.handleStartTime(e, comp)
 
 		case 3: // On the start line
-			log := fmt.Sprintf("The competitor(%d) is on the start line", e.CompetitorID)
-			p.AddLog(e.Time, log)
+			p.handleOnStartLine(e, comp)
 
 		case 4: // Started
-			comp.ActualStart = e.Time
-
-			startWindow := comp.PlannedStart.Add(p.Config.StartDelta)
-
-			if comp.ActualStart.After(startWindow) {
-				log := fmt.Sprintf("The competitor(%d) is disqualified", e.CompetitorID)
-				p.AddLog(e.Time, log)
-			} else {
-				log := fmt.Sprintf("The competitor(%d) has started", e.CompetitorID)
-				comp.NotStarted = false
-				p.AddLog(e.Time, log)
-			}
+			p.handleStarted(e, comp)
 
 		case 5: // On the firing range
-			comp.CurrentHits = 0
-			if len(e.ExtraParams) == 1 {
-				firingRange := e.ExtraParams[0]
-				log := fmt.Sprintf("The competitor(%d) is on the firing range(%s)", e.CompetitorID, firingRange)
-				p.AddLog(e.Time, log)
-			}
+			p.handleOnFiringRange(e, comp)
 
 		case 6: // Hit
-			comp.TotalHits++
-			comp.CurrentHits++
-			if len(e.ExtraParams) >= 1 {
-				target := e.ExtraParams[0]
-				log := fmt.Sprintf("The target(%s) has been hit by competitor(%d)", target, e.CompetitorID)
-				p.AddLog(e.Time, log)
-			}
+			p.handleHit(e, comp)
 
 		case 7: // Left the firing range
-			log := fmt.Sprintf("The competitor(%d) left the firing range", e.CompetitorID)
-			p.AddLog(e.Time, log)
+			p.handleLeftFiringRange(e, comp)
 
 		case 8: // Entered the penalty lap(s)
-
-			comp.EnterPenalty(e.Time)
-
-			log := fmt.Sprintf("The competitor(%d) entered the penalty laps", e.CompetitorID)
-			p.AddLog(e.Time, log)
+			p.handleEnteredPLaps(e, comp)
 
 		case 9: // Left the penalty lap(s)
-			missedShots := SHOTS_PER_FIRING_LINE - comp.CurrentHits
-			pLen := missedShots * p.Config.PenaltyLen
-			comp.ExitPenalty(e.Time, pLen)
-
-			log := fmt.Sprintf("The competitor(%d) left the penalty laps", e.CompetitorID)
-			p.AddLog(e.Time, log)
+			p.handleLeftPLaps(e, comp)
 
 		case 10: // Ended the main lap
-			comp.EndLap(e.Time)
-
-			// Finished
-			if len(comp.LapDurations) == p.Config.Laps {
-				comp.NotFinished = false
-				comp.FinishTime = e.Time
-			}
-
-			log := fmt.Sprintf("The competitor(%d) ended the main lap", e.CompetitorID)
-			p.AddLog(e.Time, log)
+			p.handleEndedMainLap(e, comp)
 
 		case 11: // Cant continue
-			comment := ""
-			if len(e.ExtraParams) > 0 {
-				for _, word := range e.ExtraParams {
-					comment += word + " "
-				}
-			}
-			log := fmt.Sprintf("The competitor can`t continue: %s", comment)
-			p.AddLog(e.Time, log)
+			p.handleCantContinue(e, comp)
 		}
 	}
 
@@ -168,57 +109,20 @@ func (p *Processor) GenerateResults() []string {
 		return ci.TotalDuration < cj.TotalDuration
 	})
 
-	totalShots := SHOTS_PER_FIRING_LINE * p.Config.FiringLines
 	results := []string{}
 
 	for _, c := range comps {
 		entry := ""
 
-		switch {
-		case c.NotStarted:
-			entry += "[NotStarted] "
+		entry += p.parseTimeAndStatus(c)
 
-		case c.NotFinished:
-			entry += "[NotFinished] "
+		entry += p.parseID(c)
 
-		default:
-			entry += fmt.Sprintf("[%v] ", formatDuration(c.TotalDuration))
-		}
+		entry += p.parseMainLaps(c)
 
-		entry += fmt.Sprintf("%d [", c.ID)
+		entry += p.parsePLaps(c)
 
-		if len(c.LapDurations) > 0 {
-			//TODO fix checking
-			d := c.LapDurations[0]
-			avgSpeed := float64(p.Config.LapLen) / d.Seconds()
-			entry += fmt.Sprintf("{%s, %.3f}", formatDuration(d), avgSpeed)
-
-			if len(c.LapDurations) > 1 {
-				for _, d := range c.LapDurations[1:] {
-					avgSpeed := float64(p.Config.LapLen) / d.Seconds()
-					entry += fmt.Sprintf(", {%s, %.3f}", formatDuration(d), avgSpeed)
-				}
-			}
-		}
-
-		if len(c.LapDurations) < p.Config.Laps {
-			for i := len(c.LapDurations) + 1; i <= p.Config.Laps; i++ {
-				if i == 1 {
-					entry += "{,}"
-				} else {
-					entry += ", {,}"
-				}
-			}
-		}
-		entry += "] "
-		if c.TotalPenaltyLen > 0 {
-			avgPenSpeed := float64(c.TotalPenaltyLen) / c.TotalPenaltyTime.Seconds()
-			entry += fmt.Sprintf("{%s, %.3f} ", formatDuration(c.TotalPenaltyTime), avgPenSpeed)
-		} else {
-			entry += "{,} "
-		}
-
-		entry += fmt.Sprintf("%d/%d", c.TotalHits, totalShots)
+		entry += p.parseHitsAndShots(c)
 
 		results = append(results, entry)
 	}
@@ -232,4 +136,169 @@ func formatDuration(d time.Duration) string {
 	millis := int(d.Milliseconds()) % 1000
 
 	return fmt.Sprintf("%02d:%02d:%02d.%03d", hours, minutes, seconds, millis)
+}
+
+func (p *Processor) handleRegistration(e *event.Event, _ *competitor.Competitor) {
+	log := fmt.Sprintf("The competitor(%d) registered", e.CompetitorID)
+	p.AddLog(e.Time, log)
+}
+
+func (p *Processor) handleStartTime(e *event.Event, comp *competitor.Competitor) {
+	if len(e.ExtraParams) == 1 {
+		startTime := e.ExtraParams[0]
+		parsedStartTime, err := time.Parse("15:04:05.000", startTime)
+		comp.CurLapStart = parsedStartTime
+		if err == nil {
+			comp.PlannedStart = parsedStartTime
+			log := fmt.Sprintf("The start time of competitor(%d) was set by a draw to %s", e.CompetitorID, startTime)
+			p.AddLog(e.Time, log)
+		}
+	}
+}
+
+func (p *Processor) handleOnStartLine(e *event.Event, _ *competitor.Competitor) {
+	log := fmt.Sprintf("The competitor(%d) is on the start line", e.CompetitorID)
+	p.AddLog(e.Time, log)
+}
+
+func (p *Processor) handleStarted(e *event.Event, comp *competitor.Competitor) {
+	comp.ActualStart = e.Time
+
+	startWindow := comp.PlannedStart.Add(p.Config.StartDelta)
+
+	if comp.ActualStart.After(startWindow) {
+		log := fmt.Sprintf("The competitor(%d) is disqualified", e.CompetitorID)
+		p.AddLog(e.Time, log)
+	} else {
+		log := fmt.Sprintf("The competitor(%d) has started", e.CompetitorID)
+		comp.NotStarted = false
+		p.AddLog(e.Time, log)
+	}
+}
+
+func (p *Processor) handleOnFiringRange(e *event.Event, comp *competitor.Competitor) {
+	comp.CurrentHits = 0
+	if len(e.ExtraParams) == 1 {
+		firingRange := e.ExtraParams[0]
+		log := fmt.Sprintf("The competitor(%d) is on the firing range(%s)", e.CompetitorID, firingRange)
+		p.AddLog(e.Time, log)
+	}
+}
+
+func (p *Processor) handleHit(e *event.Event, comp *competitor.Competitor) {
+	comp.TotalHits++
+	comp.CurrentHits++
+	if len(e.ExtraParams) >= 1 {
+		target := e.ExtraParams[0]
+		log := fmt.Sprintf("The target(%s) has been hit by competitor(%d)", target, e.CompetitorID)
+		p.AddLog(e.Time, log)
+	}
+}
+
+func (p *Processor) handleLeftFiringRange(e *event.Event, _ *competitor.Competitor) {
+	log := fmt.Sprintf("The competitor(%d) left the firing range", e.CompetitorID)
+	p.AddLog(e.Time, log)
+}
+
+func (p *Processor) handleEnteredPLaps(e *event.Event, comp *competitor.Competitor) {
+	comp.EnterPenalty(e.Time)
+
+	log := fmt.Sprintf("The competitor(%d) entered the penalty laps", e.CompetitorID)
+	p.AddLog(e.Time, log)
+}
+
+func (p *Processor) handleLeftPLaps(e *event.Event, comp *competitor.Competitor) {
+	missedShots := SHOTS_PER_FIRING_LINE - comp.CurrentHits
+	pLen := missedShots * p.Config.PenaltyLen
+	comp.ExitPenalty(e.Time, pLen)
+
+	log := fmt.Sprintf("The competitor(%d) left the penalty laps", e.CompetitorID)
+	p.AddLog(e.Time, log)
+}
+
+func (p *Processor) handleEndedMainLap(e *event.Event, comp *competitor.Competitor) {
+	comp.EndLap(e.Time)
+
+	// Finished
+	if len(comp.LapDurations) == p.Config.Laps {
+		comp.NotFinished = false
+		comp.FinishTime = e.Time
+	}
+
+	log := fmt.Sprintf("The competitor(%d) ended the main lap", e.CompetitorID)
+	p.AddLog(e.Time, log)
+}
+
+func (p *Processor) handleCantContinue(e *event.Event, _ *competitor.Competitor) {
+	comment := ""
+	if len(e.ExtraParams) > 0 {
+		for _, word := range e.ExtraParams {
+			comment += word + " "
+		}
+	}
+	log := fmt.Sprintf("The competitor can`t continue: %s", comment)
+	p.AddLog(e.Time, log)
+}
+
+func (p *Processor) parseMainLaps(c *competitor.Competitor) string {
+	res := "["
+	if len(c.LapDurations) > 0 {
+		d := c.LapDurations[0]
+		avgSpeed := float64(p.Config.LapLen) / d.Seconds()
+		res += fmt.Sprintf("{%s, %.3f}", formatDuration(d), avgSpeed)
+
+		if len(c.LapDurations) > 1 {
+			for _, d := range c.LapDurations[1:] {
+				avgSpeed := float64(p.Config.LapLen) / d.Seconds()
+				res += fmt.Sprintf(", {%s, %.3f}", formatDuration(d), avgSpeed)
+			}
+		}
+	}
+
+	if len(c.LapDurations) < p.Config.Laps {
+		for i := len(c.LapDurations) + 1; i <= p.Config.Laps; i++ {
+			if i == 1 {
+				res += "{,}"
+			} else {
+				res += ", {,}"
+			}
+		}
+	}
+	res += "] "
+	return res
+}
+
+func (p *Processor) parseTimeAndStatus(c *competitor.Competitor) string {
+	res := ""
+	switch {
+	case c.NotStarted:
+		res += "[NotStarted] "
+
+	case c.NotFinished:
+		res += "[NotFinished] "
+
+	default:
+		res += fmt.Sprintf("[%v] ", formatDuration(c.TotalDuration))
+	}
+	return res
+}
+
+func (p *Processor) parsePLaps(c *competitor.Competitor) string {
+	res := ""
+	if c.TotalPenaltyLen > 0 {
+		avgPenSpeed := float64(c.TotalPenaltyLen) / c.TotalPenaltyTime.Seconds()
+		res += fmt.Sprintf("{%s, %.3f} ", formatDuration(c.TotalPenaltyTime), avgPenSpeed)
+	} else {
+		res += "{,} "
+	}
+	return res
+}
+
+func (p *Processor) parseHitsAndShots(c *competitor.Competitor) string {
+	totalShots := SHOTS_PER_FIRING_LINE * p.Config.FiringLines
+	return fmt.Sprintf("%d/%d", c.TotalHits, totalShots)
+}
+
+func (p *Processor) parseID(c *competitor.Competitor) string {
+	return fmt.Sprintf("%d ", c.ID)
 }
